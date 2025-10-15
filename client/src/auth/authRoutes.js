2cdc55e6912b1d5e5042e0b7705981d616c5a8a7
Prince Patial <princepatial20@gmail.com>
2025-10-15 10:36:30 +0530

    fix .env path

--- client/src/auth/authRoutes.js
+++ client/src/auth/authRoutes.js
@@ -2,9 +2,9 @@ export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 
 export const USER_ENDPOINTS = {
-  LOGIN_USER: `${API_BASE_URL}/login`,
-  VERIFY_USER: `${API_BASE_URL}/verifyToken`,
-  LOGOUT_USER: `${API_BASE_URL}/logout`,
+  LOGIN_USER: `${API_BASE_URL}/users/login`,
+  VERIFY_USER: `${API_BASE_URL}/users/verifyToken`,
+  LOGOUT_USER: `${API_BASE_URL}/users/logout`,
 };
 
 
