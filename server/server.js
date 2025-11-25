e973397d784b6cd29a3ba8022e05e74daab9adc1
Prince Patial <princepatial20@gmail.com>
2025-11-25 19:07:02 +0530

    image big issue size fix

--- server/server.js
+++ server/server.js
@@ -18,6 +18,11 @@ const app = express();
 app.set('trust proxy', true);
 app.use(cookieParser());
 
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+
+
 // Middleware
 app.use(express.json());
 app.use(cors({
